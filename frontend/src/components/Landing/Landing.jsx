76cf67dbd7fc0a62426941741a7b3bb9b5b4bb6c
Houlaymatou B <houlaymatoub76@gmail.com>
2025-01-10 19:10:13 -0600

    update: apple touch icon

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -36,7 +36,7 @@ const LandingPage = () => {
         <h1>LitMood</h1>
         <p>
           Feeling a vibe? Let LitMood turn your mood into the perfect read. Dive
-          into stories that match your feels, every single time.
+          into stories that match your feelings, every single time.
         </p>
         <button onClick={handleNavigation} disabled={isAnimating}>
           Try Now 📚
